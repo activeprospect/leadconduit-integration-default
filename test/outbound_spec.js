edbade6bb743a666eaabe1dec8324decd321d44b
Chris Grayson <cgrayson@airpost.net>
2021-06-04 10:07:52 -0500

    update to use dev-dependencies, plus straightforward lint fixes

--- test/outbound_spec.js
+++ test/outbound_spec.js
@@ -10,82 +10,66 @@ const requestVars = integration.request.variables().concat([
 
 const variables = require('../test/helper').variables(requestVars);
 
-
 describe('Outbound Request', () => {
-
-
   it('should send accept header', () => assert.equal(integration.request(variables()).headers.Accept, 'application/json;q=0.9,text/xml;q=0.8,application/xml;q=0.7'));
 
-
   it('should encode content sent via get as querystring', () => {
-    const { url } =  integration.request(variables({method: 'get'}));
+    const { url } = integration.request(variables({ method: 'get' }));
     assert.equal(url, 'http://externalservice/?first_name=Joe&last_name=Blow&email=jblow%40test.com&phone_1=5127891111&price=1.5');
   });
 
-
   it('should merge content sent via get over querystring', () => {
-    const req = integration.request(variables({url: 'http://externalservice?first_name=Bobby&aff_id=123', method: 'get'})).url;
+    const req = integration.request(variables({ url: 'http://externalservice?first_name=Bobby&aff_id=123', method: 'get' })).url;
     assert.equal(req, 'http://externalservice/?first_name=Joe&aff_id=123&last_name=Blow&email=jblow%40test.com&phone_1=5127891111&price=1.5');
   });
 
-
   it('should handle null variable', () => {
-    const { url } = integration.request(variables({lead: { first_name: null }, method: 'get'}));
+    const { url } = integration.request(variables({ lead: { first_name: null }, method: 'get' }));
     assert.equal(url, 'http://externalservice/?first_name=&last_name=Blow&email=jblow%40test.com&phone_1=5127891111&price=1.5');
   });
 
-
   it('should handle undefined variable', () => {
-    const { url } = integration.request(variables({lead: { first_name: undefined }, method: 'get'}));
+    const { url } = integration.request(variables({ lead: { first_name: undefined }, method: 'get' }));
     assert.equal(url, 'http://externalservice/?last_name=Blow&email=jblow%40test.com&phone_1=5127891111&price=1.5');
   });
-
 
   it('should encode content sent as post', () => {
     const { body } = integration.request(variables());
     assert.equal(body, 'first_name=Joe&last_name=Blow&email=jblow%40test.com&phone_1=5127891111&price=1.5');
   });
 
-
   it('should set content length of post', () => assert.equal(integration.request(variables()).headers['Content-Length'], 81));
-
 
   it('should set content type of post', () => assert.equal(integration.request(variables()).headers['Content-Type'], 'application/x-www-form-urlencoded'));
 
-
   it('should handle dot notation vars', () => {
-    const { url } = integration.request({url: 'http://externalservice', method: 'get', lead: { 'deeply.nested.var': 'Hola' }});
+    const { url } = integration.request({ url: 'http://externalservice', method: 'get', lead: { 'deeply.nested.var': 'Hola' } });
     assert.equal(url, 'http://externalservice/?deeply.nested.var=Hola&price=0');
   });
-
 
   it('should handle deeply nested vars', () => {
-    const { url } = integration.request({url: 'http://externalservice', method: 'get', lead: { deeply: { nested: { var: 'Hola' } } }});
+    const { url } = integration.request({ url: 'http://externalservice', method: 'get', lead: { deeply: { nested: { var: 'Hola' } } } });
     assert.equal(url, 'http://externalservice/?deeply.nested.var=Hola&price=0');
   });
 
-
   it('should handle new format custom fields', () => {
-    const { body } = integration.request(variables({default: {custom: {favorite_color: 'pink'}}}));
+    const { body } = integration.request(variables({ default: { custom: { favorite_color: 'pink' } } }));
     assert.equal(body, 'first_name=Joe&last_name=Blow&email=jblow%40test.com&phone_1=5127891111&favorite_color=pink&price=1.5');
   });
 
   it('should overwrite standard fields with custom fields of the same name', () => {
-    const { body } = integration.request(variables({default: {custom: {email: 'custom@email.com'}}}));
+    const { body } = integration.request(variables({ default: { custom: { email: 'custom@email.com' } } }));
     assert.equal(body, 'first_name=Joe&last_name=Blow&email=custom%40email.com&phone_1=5127891111&price=1.5');
   });
 });
 
 describe('Outbound Validate', () => {
-
   it('should require valid default_outcome', () => {
-    const vars = variables({default_outcome: 'donkey'});
+    const vars = variables({ default_outcome: 'donkey' });
     assert.equal(integration.validate(vars), 'default outcome must be "success", "failure" or "error"');
   });
 
-
-  it('should allow valid default_outcome', () => assert.isUndefined(integration.validate(variables({default_outcome: 'success'}))));
-
+  it('should allow valid default_outcome', () => assert.isUndefined(integration.validate(variables({ default_outcome: 'success' }))));
 
   it('should require url variable', () => {
     const vars = variables();
@@ -93,31 +77,22 @@ describe('Outbound Validate', () => {
     assert.equal(integration.validate(vars), 'URL is required');
   });
 
-
   it('should require valid url variable', () => {
     const vars = variables();
     vars.url = 'donkeykong';
     assert.equal(integration.validate(vars), 'URL must be valid');
   });
 
-
   it('should not allow head', () => assertMethodNotAllowed('head'));
-
 
   it('should not allow put', () => assertMethodNotAllowed('put'));
 
-
   it('should not allow delete', () => assertMethodNotAllowed('delete'));
-
 
   it('should not allow patch', () => assertMethodNotAllowed('patch'));
 });
 
-
-
-
 describe('Outbound Response', () => {
-
   let vars;
   let req;
   let res;
@@ -134,21 +109,18 @@ describe('Outbound Response', () => {
     req = integration.request(vars);
   });
 
-
   it('should default outcome to "error"', () => {
     const event = integration.response(vars, req, res);
     assert.equal(event.outcome, 'error');
     assert.equal(event.reason, 'Unrecognized response');
   });
 
-
   it('should default outcome to specified default', () => {
-    vars = variables({default_outcome: 'success'});
+    vars = variables({ default_outcome: 'success' });
     req = integration.request(vars);
     const event = integration.response(vars, req, res);
     assert.equal(event.outcome, 'success');
   });
-
 
   it('should preserve existing reason even if outcome defaults to "error"', () => {
     res.body = '{"id":42,"reason": "Big bada boom"}';
@@ -156,8 +128,6 @@ describe('Outbound Response', () => {
     assert.equal(event.outcome, 'error');
     assert.equal(event.reason, 'Big bada boom');
   });
-
-
 
   it('should parse XML response', () => {
     res.headers['Content-Type'] = 'text/xml';
@@ -177,15 +147,13 @@ describe('Outbound Response', () => {
     assert.deepEqual(event, expected);
   });
 
-
   it('should handle poorly formed XML response', () => {
     // uses sample invalid XML from customer
     res.headers['Content-Type'] = 'text/xml';
     res.body = '<status>Error</status><reason>Please send in the mg_site_id and mg_cid as part of your request. Request Parameter = mg_site_id</reason>';
     const event = integration.response(vars, req, res);
-    assert.deepEqual(event, {outcome: 'error', reason: 'Unrecognized response'});
+    assert.deepEqual(event, { outcome: 'error', reason: 'Unrecognized response' });
   });
-
 
   it('should parse JSON response', () => {
     const expected = {
@@ -203,7 +171,6 @@ describe('Outbound Response', () => {
     const event = integration.response(vars, req, res);
     assert.deepEqual(event, expected);
   });
-
 
   it('should use handler "message" as error reason', () => {
     const expected = {
@@ -224,15 +191,13 @@ describe('Outbound Response', () => {
   });
 });
 
-
-var assertMethodNotAllowed = function(method) {
+var assertMethodNotAllowed = function (method) {
   const vars = variables();
   vars.method = method;
   try {
     assert.equal(integration.validate(vars), `Unsupported HTTP method ${method.toUpperCase()}. Use GET or POST.`);
   } catch (error) {}
 };
-
 
 var xmlBody = () =>
   `\
@@ -249,4 +214,3 @@ var xmlBody = () =>
 </result>\
 `
 ;
-
